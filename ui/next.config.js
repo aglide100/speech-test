62ec7d9eaa94e6dccec247962a9e71c617a5c48b
HeeJun Moon <aglide100@gmail.com>
2023-10-31 22:26:21 +0900

    modify workflow

--- ui/next.config.js
+++ ui/next.config.js
@@ -2,27 +2,27 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const nextConfig = {
+    reactStrictMode: true,
     async rewrites() {
         return [
             {
-                source: "/:path*",
-                destination: `https://` + `/:path*`,
+                source: "/data/:path*",
+                destination: `http://localhost:9090` + `/:path*`,
             },
         ];
     },
-    async serverMiddleware() {
-        const apiProxy = createProxyMiddleware("/", {
-            target: `https://`,
-            changeOrigin: true,
-            secure: true,
-            protocolRewrite: "https",
-            onProxyReq: (proxyReq) => {
-                proxyReq.setHeader("Connection", "keep-alive");
-            },
-        });
-
-        return [apiProxy];
-    },
+    // async serverMiddleware() {
+    //     const apiProxy = createProxyMiddleware("/", {
+    //         target: `https://`,
+    //         changeOrigin: true,
+    //         secure: true,
+    //         protocolRewrite: "https",
+    //         onProxyReq: (proxyReq) => {
+    //             proxyReq.setHeader("Connection", "keep-alive");
+    //         },
+    //     });
+    //     return [apiProxy];
+    // },
 };
 
 module.exports = nextConfig;
--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,11 +1,40 @@
+"use client";
 import Image from "next/image";
-import Player from "@/component/\bPlayer/Player";
+import React, { useRef, useState } from "react";
+import ReactHlsPlayer from "react-hls-player";
 
 export default function Home() {
+    const playerRef = useRef<HTMLVideoElement | null>(null);
+    const [index, setIndex] = useState("39");
+    const [url, setUrl] = useState("/data/playlist/" + index);
+
+    const onChange = (e: any) => {
+        setIndex(e.target.value);
+    };
+
+    const whenClick = () => {
+        setUrl("/data/playlist/" + index);
+    };
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
-            <Player videoUrl="localhost:8080/test"></Player>
+            <div>
+                <input
+                    onChange={onChange}
+                    value={index}
+                    className="text-black"
+                />
+                <button onClick={whenClick}>set</button>
+            </div>
+            <ReactHlsPlayer
+                autoPlay={false}
+                controls={true}
+                width="100%"
+                height="auto"
+                src={url}
+                playerRef={playerRef}
+            ></ReactHlsPlayer>
         </main>
     );
 }
